9525cee6d14c2bab73b5ccbc2b3157f577913381
kholifah-dina <136870671+kholifah-dina@users.noreply.github.com>
2023-06-19 21:44:34 +0700

    Update Tubes.py

--- Tubes.py
+++ Tubes.py
@@ -7,7 +7,29 @@ def insertion_sort(arr):
             j -= 1
         arr[j + 1] = key
 
-#tambahan cuy
+def daftar_film():
+    print("Daftar Film: ")
+    print("1. Sewu Dino (Rp. 30.000)")
+    print("2. The Little Mermaid (Rp. 35.000)")
+    print("3. The Big 4 (Rp. 40.000)")
+    print("4. Hati Suhita (Rp. 30.000)")
+    
+def waktu_tayang(film):
+    waktu_tayang = {
+        "1": ["08:00", "16:00", "20:00"],
+        "2": ["10:00", "13:00", "20:00"],
+        "3": ["22:00", "15:00", "09:00"],
+        "4": ["08:00", "20:00"]
+    }
+    
+waktu = waktu_tayang.get(film)
+if waktu is not None:
+    insertion_sort(waktu)
+    print("Jam Tayang: ")
+    for i, time in enumerate(times):
+       print(f"{i + 1}. {time}")
+else:
+    print("Film tidak ada.")
 
 #coba aja
 #tambah aja
